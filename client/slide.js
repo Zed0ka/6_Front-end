bf112b56bc4052971b945bd7b7ec5e40aff4e642
Zed0ka <115066855+Zed0ka@users.noreply.github.com>
2023-04-02 19:32:54 +0200

    update css + html

---
+++ client/slide.js
@@ -0,0 +1,17 @@
+const clickBar = document.querySelector(".clickBar");
+const guide = document.querySelector(".landingPage");
+const navslide = document.querySelector(".navslide");
+const arrowIcon = document.querySelector(".arrowIcon");
+
+// intégrer la désactivation de la classe flashing au click
+
+clickBar.addEventListener("click",(event)=>{
+    clickBar.classList.toggle("left");              // switch de la classe "left" à "right" (et inversement sur un nouveau click)
+    clickBar.classList.toggle("right");             
+    arrowIcon.classList.toggle("fa-angles-right");  
+    arrowIcon.classList.toggle("fa-angles-left");   
+    guide.classList.toggle("visible");              
+    guide.classList.toggle("hidden");               
+    navslide.classList.toggle("hidden");            
+    navslide.classList.toggle("visible");           
+})
