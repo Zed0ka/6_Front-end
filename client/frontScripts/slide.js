404664fb05b4195487004c128bb917fe6564f348
Zed0ka <115066855+Zed0ka@users.noreply.github.com>
2023-04-05 09:55:08 +0200

    update readme checklist

--- client/frontScripts/slide.js
+++ client/frontScripts/slide.js
@@ -6,7 +6,7 @@ const arrowIcon = document.querySelector(".arrowIcon");
 // intégrer la désactivation de la classe flashing au click
 
 clickBar.addEventListener("click",(event)=>{
-    clickBar.classList.toggle("left");              // switch de la classe "left" à "right" (et inversement sur un nouveau click)
+    clickBar.classList.toggle("left");              //!switch de la classe "left" à "right" (et inversement sur un nouveau click) PAS BON
     clickBar.classList.toggle("right");             
     arrowIcon.classList.toggle("fa-angles-right");  
     arrowIcon.classList.toggle("fa-angles-left");   
